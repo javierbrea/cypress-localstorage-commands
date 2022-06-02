90fd39e2ac53ca72f31a36c7a8556f77ae247a50
javierbrea <javier.brea@gmail.com>
2022-06-02 12:07:01 +0200

    test: Use Cypress v10 for running plugin E2E tests

--- test-e2e/react-app/cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-const webpackPreprocessor = require("@cypress/webpack-preprocessor");
-const defaults = webpackPreprocessor.defaultOptions;
-
-module.exports = (on) => {
-  delete defaults.webpackOptions.module.rules[0].use[0].options.presets;
-  on("file:preprocessor", webpackPreprocessor(defaults));
-};
